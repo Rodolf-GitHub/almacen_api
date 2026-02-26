18baab859dd7e51d7e4839e9a8a63639f6f63925
Rodolfo <rodolfogroero2@gmail.com>
2026-02-26 16:46:12 -0300

    Agregar comentarios para admin_general y asegurar el formato de las clases de esquema en usuario

--- usuario/schemas.py
+++ usuario/schemas.py
@@ -6,12 +6,14 @@ class Usuario(ModelSchema):
         model = 'usuario.Usuario'
         exclude = ['contrasena_hasheada', 'token']
 
+# solo para admin_general
 class UsuarioCreate(Schema):
     nombre: str
     contrasena: str
     nombre_sucursal: str
     rol: Optional[str] = 'admin_sucursal'
 
+# solo para admin_general
 class UsuarioUpdate(Schema):
     nombre: Optional[str]
     contrasena: Optional[str]
@@ -22,3 +24,11 @@ class CambiarMiContrasena(Schema):
     contrasena_actual: str
     contrasena_nueva: str
     repetir_contrasena_nueva: str
+
+class LoginRequest(Schema):
+    nombre: str
+    contrasena: str
+
+class TokenResponse(Schema):
+    token: str
+    usuario : Usuario
