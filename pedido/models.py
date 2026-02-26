5b911a05c72214695cbee9542bf4bacd36f7da6a
Rodolfo <rodolfogroero2@gmail.com>
2026-02-26 16:39:11 -0300

    Agregar elecciones de estado en el modelo Pedido y corregir nombre de campo en Usuario

--- pedido/models.py
+++ pedido/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from core.models import BaseModel
 # Create your models here.
 class Pedido(BaseModel):
-    creado_por = models.ForeignKey('usuario.Usuario', on_delete=models.SET_NULL, null=True, blank=True)
+    class EstadoChoices(models.TextChoices):
+        PENDIENTE = 'pendiente', 'Pendiente'
+        COMPLETADO = 'completado', 'Completado'
+
+    creado_por = models.ForeignKey('usuario.Usuario', on_delete=models.CASCADE, related_name='pedidos_creados')
     usuario_destino = models.ForeignKey('usuario.Usuario', on_delete=models.SET_NULL, null=True, blank=True, related_name='pedidos_recibidos')
-    estado = models.CharField(max_length=20, default='pendiente')# pendiente,completado
+    estado = models.CharField(max_length=20, choices=EstadoChoices.choices, default=EstadoChoices.PENDIENTE)
 
 class PedidoDetalle(BaseModel):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='detalles')
--- usuario/models.py
+++ usuario/models.py
@@ -3,10 +3,13 @@ from core.models import BaseModel
 # Create your models here.
 class Usuario(BaseModel):
     nombre = models.CharField(max_length=15,unique=True)
-    contraseña = models.CharField(max_length=100)
+    contrasena_hasheada = models.CharField(max_length=100)
     nombre_sucursal = models.CharField(max_length=100)
     token = models.CharField(max_length=128, blank=True, null=True)
-    rol = models.CharField(max_length=20, default='admin_sucursal')
+    class RolChoices(models.TextChoices):
+        ADMIN_GENERAL = 'admin_general', 'Admin General'
+        ADMIN_SUCURSAL = 'admin_sucursal', 'Admin Sucursal'
+    rol = models.CharField(max_length=20, choices=RolChoices.choices, default=RolChoices.ADMIN_SUCURSAL)
 
     class Meta:
         db_table = 'usuario'
